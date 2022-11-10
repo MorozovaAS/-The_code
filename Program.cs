467340b856db40305ceccb1f5b68cc33920e4031
Анна Морозова <anya_shukost@mail.ru>
2022-11-10 12:02:56 +0500

    код

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿System.Console.WriteLine("Введите массив через запятую: ");
+string words = Console.ReadLine();
+string[] existArr = words.Split(", ");
+string[] resultArr = existArr.Where(words=>words.Length < 4).ToArray();
+System.Console.WriteLine(String.Join(", ", resultArr));
